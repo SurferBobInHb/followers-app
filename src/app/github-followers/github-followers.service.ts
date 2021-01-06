221ee5bdcc9aec4b62b63921ddf558c683fa7704
bob stubbs <bob.stubbs.hb@gmail.com>
2021-01-06 09:38:25 -0800

    init 2

--- src/app/github-followers/github-followers.service.ts
+++ src/app/github-followers/github-followers.service.ts
@@ -1,15 +1,17 @@
+/* eslint-disable linebreak-style */
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map'; 
+import { Observable } from 'rxjs';
 
-@Injectable()
-export class GithubFollowersService {
+@Injectable() 
+export class GithubFollowersService { 
   private readonly _url = 'https://api.github.com/users/mosh-hamedani/followers';
 
   constructor(private http: Http) {  }
 
-  getFollowers() { 
-    return this.http.get(this._url)
-      .map(response => response.json());
+  getFollowers() : Observable<string> {  
+      return this.http.get(this._url)
+          .map(response => response.json()); 
   }
 }
